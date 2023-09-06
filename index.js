f57d05d0ec8b5dd2873a860eca3be2072f498f0c
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-09-06 15:37:49 -0300

    terminando projeto

--- index.js
+++ index.js
@@ -35,6 +35,18 @@ client.login(TOKEN);
 
 // Listener de interações
 client.on(Events.InteractionCreate, async interaction => {
+  if (interaction.isStringSelectMenu()) {
+    const selected = interaction.values[0];
+    if (selected === 'javascript') {
+      await interaction.reply("Documentação do Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript/");
+    } else if (selected === 'python') {
+      await interaction.reply("Documentação do Python: https://www.python.org/");
+    } else if (selected == "csharp") {
+      await interaction.reply("Documentação do C#: https://learn.microsoft.com/en-us/dotnet/csharp/");
+    } else if (selected == "discordjs") {
+      await interaction.reply("Documentação do Discord.js: https://discordjs.guide/#before-you-begin");
+    };
+  }
   if (!interaction.isChatInputCommand()) return
     const command = interaction.client.commands.get(interaction.commandName);
     if (!command) {
